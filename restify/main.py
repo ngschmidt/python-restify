a7a5d5f4f9a8b13247d2207030c984fe8266386b
Nicholas G. Schmidt <nick.schmidt3@gmail.com>
2021-11-14 20:44:50 -0900

    working code

--- restify/main.py
+++ restify/main.py
@@ -65,7 +65,6 @@ elif args.getplay or args.dryrun:
         if not args.vars:
             cogitation_interface.namshub(args.play, namshub_dryrun=True)
         else:
-            print(args.vars)
             cogitation_interface.namshub(args.play, namshub_variables=json.loads(args.vars), namshub_dryrun=True)
 else:
     # Provide an "overloading interface"
