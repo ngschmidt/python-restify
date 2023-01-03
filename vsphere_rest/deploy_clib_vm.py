1f3f46e1c011f33b884cbc141a923ad0616f836d
Nick Schmidt <nick.schmidt3@gmail.com>
2023-01-02 19:47:37 -0900

    working stripped api key enrollment

--- vsphere_rest/deploy_clib_vm.py
+++ vsphere_rest/deploy_clib_vm.py
@@ -27,10 +27,13 @@ for env_mandatory in ["API_ENDPOINT", "API_USER", "API_PASS"]:
 cogitation_interface = Reliquary(
     "settings.json",
     input_user=os.getenv("API_USER"),
-    input_pass=os.getenv("API_PASS", input_endpoint=os.getenv("API_ENDPOINT")),
+    input_pass=os.getenv("API_PASS"),
+    input_endpoint=os.getenv("API_ENDPOINT"),
 )
+
+# Get the API key and add it to the API interface object
 cogitation_interface.add_http_header(
-    "vmware-api-session-id", cogitation_interface.namshub("post_api_key")
+    "vmware-api-session-id", cogitation_interface.namshub("post_api_key").strip("\"")
 )
 print(json.dumps(cogitation_interface.cogitation_headers))
 
